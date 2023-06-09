99a5c23921b12d6c66926e74fb4295bc459cffd9
Urinov Farrukh <UrinovFarrukh27@icloud.com>
2023-06-09 19:44:48 +0500

    Initial Commit

---
+++ Lesson-39. Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-39. Exam
+//
+//  Created by User on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
