762fdd1532f2d8dfee5981538dc80339551a2579
Safojon Axmadov <safojonaxmadov85@gmail.com>
2023-09-21 20:15:08 +0500

    first commit

---
+++ Lesson-39. Exam/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Lesson-39. Exam
+//
+//  Created by Safojon Axmadov on 18/08/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Lesson-39. Exam/TaskModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskModel.swift
+//  Lesson-39. Exam
+//
+//  Created by Safojon Axmadov on 18/08/23.
+//
+
+import Foundation
